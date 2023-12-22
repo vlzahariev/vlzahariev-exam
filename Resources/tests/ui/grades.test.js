c7c7da7a25aae9fe22bed4229b44f8b9e0d95d9c
Vladimir Zahariev <vlzahariev@gmail.com>
2023-12-22 10:32:51 +0200

    tests fixing

--- Resources/tests/ui/grades.test.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/grades-list');
-    const list = await page.$('dl');
-    expect(list).toBeTruthy();
-  });
-  
