655561677f7901fef5dfedf3c3cbaf5cae96b408
Vladimir Zahariev <vlzahariev@gmail.com>
2023-12-22 11:24:11 +0200

    UI tests add

--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8090/my-grades');
+    await page.goto('https://vlzahariev-exam.onrender.com/my-grades');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
